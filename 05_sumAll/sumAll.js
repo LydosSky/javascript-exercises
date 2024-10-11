5ce28899a485eb1f01ab8b7094941be0f8f2ec3f
Lydos <lydossky@gmail.com>
2024-10-11 19:59:32 +0300

    fourth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const validate = (value) => Number.isInteger(value) && value >= 0;
 
+const sumAll = function (left, right) {
+  return validate(left) && validate(right)
+    ? left === right
+      ? right
+      : left > right
+        ? right + sumAll(left, right + 1)
+        : left + sumAll(left + 1, right)
+    : "ERROR";
 };
 
 // Do not edit below this line
