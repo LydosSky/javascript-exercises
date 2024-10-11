b153f9952d6ab4ebdc4988d42826e176fc04c72b
Lydos <lydossky@gmail.com>
2024-10-11 16:18:30 +0300

    first exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  return number < 0
+    ? "ERROR"
+    : number === 0
+      ? ""
+      : number === 1
+        ? string
+        : string + repeatString(string, number - 1);
 };
 
 // Do not edit below this line
