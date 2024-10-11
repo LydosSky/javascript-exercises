95fa896497ef0765b9abab43fd0bdc24eb11bd2d
Lydos <lydossky@gmail.com>
2024-10-11 16:33:48 +0300

    second exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let strArr = str.split("");
+  let newStr = [];
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    newStr.push(strArr[i]);
+  }
 
+  return newStr.join("");
 };
 
 // Do not edit below this line
