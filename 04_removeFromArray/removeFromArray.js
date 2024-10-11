10bef369a1f9a1349f539f487bda46df6472e8b1
Lydos <lydossky@gmail.com>
2024-10-11 17:27:18 +0300

    third exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemsToRemove) {
+  for (let item of itemsToRemove) {
+    let index = arr.indexOf(item);
+    while (index >= 0) {
+      arr.splice(index, 1);
+      index = arr.indexOf(item);
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
