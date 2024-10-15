b186ecd847b72e41686d1dfc0289c5d90f1c8ea8
Lydos <lydossky@gmail.com>
2024-10-15 16:52:16 +0300

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const isLetter = function (char) {
+  const charCode = char.charCodeAt();
+  return (
+    (charCode >= 65 && charCode <= 90) ||
+    (charCode >= 97 && charCode <= 122) ||
+    (charCode >= 48 && charCode <= 57)
+  );
+};
 
+const palindromes = function (string) {
+  const stringArray = string.toLowerCase().split("").filter(isLetter);
+  const stringArrayLength = stringArray.length;
+  const stringArrayMiddle = Math.floor(stringArrayLength / 2);
+
+  for (let i = 0; i < stringArrayMiddle; i++) {
+    if (stringArray[i] !== stringArray[stringArrayLength - (i + 1)]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
