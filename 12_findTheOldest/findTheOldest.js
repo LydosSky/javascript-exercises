8741c81d5a486c49d9342b815e0c38be85c6def9
Lydos <lydossky@gmail.com>
2024-10-15 19:36:33 +0300

    exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  const getAge = function (person) {
+    return person.yearOfDeath !== undefined
+      ? person.yearOfDeath - person.yearOfBirth
+      : currentYear - person.yearOfBirth;
+  };
+
+  return people.reduce((oldestPerson, currentPerson) =>
+    getAge(currentPerson) >= getAge(oldestPerson)
+      ? currentPerson
+      : oldestPerson,
+  );
 };
 
 // Do not edit below this line
