b3883b09e93518de5e6c786965ad967b02df7aa6
Lydos <lydossky@gmail.com>
2024-10-15 17:21:37 +0300

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num < 0) return "OOPS";
 
+  const helper = (first, second, num) => {
+    if (num === 0) return first;
+    return helper(second, first + second, num - 1);
+  };
+
+  return helper(0, 1, num);
 };
 
 // Do not edit below this line
